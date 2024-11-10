d1b16b5dcb225dfd9714f9b7ed42211e270ed532
Wes <wes@miragecorp.org>
2024-11-09 20:07:08 -0600

    blank Titlebar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,46 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let win_builder =
+                WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+                    .title("")
+                    .inner_size(1250.0, 750.0);
+
+            // set transparent title bar only when building for macOS
+            #[cfg(target_os = "macos")]
+            let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+
+            let window = win_builder.build().unwrap();
+
+            // set background color only when building for macOS
+            #[cfg(target_os = "macos")]
+            {
+                use cocoa::appkit::{NSColor, NSWindow};
+                use cocoa::base::{id, nil};
+
+                let ns_window = window.ns_window().unwrap() as id;
+                unsafe {
+                    let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                        nil,
+                        17.0 / 255.0,
+                        17.0 / 255.0,
+                        17.5 / 255.0,
+                        1.0,
+                    );
+                    ns_window.setBackgroundColor_(bg_color);
+                }
+            }
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

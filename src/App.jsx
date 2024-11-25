dccc5a1641391317e951f244c4e5df1242570c50
Wes <wes@miragecorp.org>
2024-11-25 12:51:48 -0500

    theme saving

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ListView, Home, Timer, Settings } from './pages';
 import { ListProvider } from './context/ListContext';
+import { load } from '@tauri-apps/plugin-store';
 import { dark } from './utils/themes';
 
 function App() {
+  const [theme, setTheme] = useState(dark);
+
+  const getTheme = async () => {
+    const store = await load('settings.json');
+    const storedTheme = await store.get('theme');
+
+    if (storedTheme) {
+      console.log('Theme loaded');
+      setTheme(storedTheme);
+    } else {
+      console.log('Theme set');
+      await store.set('theme', dark);
+      await store.save();
+    }
+  }
+
   useEffect(() => {
-      Object.keys(dark).forEach(key => { 
-          document.documentElement.style.setProperty(`--color-${key}`, dark[key]);
-      });
+    getTheme();
   }, []);
+
+  useEffect(() => {
+    Object.keys(theme).forEach(key => {
+      document.documentElement.style.setProperty(`--color-${key}`, theme[key]);
+    });
+  }, [theme]);
 
   return (
     <ListProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings" element={<Settings setTheme={setTheme} />} />
           <Route path="/list" element={<ListView />} />
           <Route path='/timer' element={<Timer />} />
         </Routes>
--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,72 +1,72 @@
 import React from 'react';
+import { load } from '@tauri-apps/plugin-store';
 import Flow from '../../components/Flow';
 import Navbar from '../../components/Navbar';
 import { dark, light, discordish, bluey, reptile } from '../../utils/themes';
 
-const Settings = () => {
+const Settings = ({ setTheme }) => {
 
-    const handleCreateList = () => {
-        createNewList('Untitled');
-        navigate('/list');
-    };
+  const handleCreateList = () => {
+    createNewList('Untitled');
+    navigate('/list');
+  };
 
-    async function setTheme(themeName) {
-        let theme;
-        switch (themeName) {
-            case 'dark':
-                theme = dark;
-                break;
-            case 'light':
-                theme = light;
-                break;
-            case 'discordish':
-                theme = discordish;
-                break;
-            case 'bluey': 
-                theme = bluey;
-                break;
-            case 'reptile':
-                theme = reptile;
-                break;
-            default:
-                console.error(`Theme ${themeName} not found`);
-                return;
-        }
-      
-        Object.keys(theme).forEach(key => {
-          document.documentElement.style.setProperty(`--color-${key}`, theme[key]);
-        });
+  async function handleSetTheme(themeName) {
+    let theme;
+    switch (themeName) {
+      case 'dark':
+        theme = dark;
+        break;
+      case 'light':
+        theme = light;
+        break;
+      case 'discordish':
+        theme = discordish;
+        break;
+      case 'bluey':
+        theme = bluey;
+        break;
+      case 'reptile':
+        theme = reptile;
+        break;
+      default:
+        console.error(`Theme ${themeName} not found`);
+        return;
     }
 
+    const store = await load('settings.json');
+    await store.set('theme', theme);
+    await store.save();
 
-    return (
-        <div className="flex flex-col w-full h-screen">
-            <div className="mb-6 mt-4 flex flex-row w-full items-center h-20 px-10">
-                <Flow />
-            </div>
+    setTheme(theme);
+  }
 
-            <div className="mx-10 flex flex-row items-center justify-between border-b border-border pb-3">
-                <h1 className="font-bold text-xl">Settings</h1>
-                <p className="font-light text-copy-light">Personalize your experience and manage your preferences</p>
-            </div>
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <div className="mb-6 mt-4 flex flex-row w-full items-center h-20 px-10">
+        <Flow />
+      </div>
 
-            {/* SET THEME */}
-            <div className="px-10 mt-8 flex flex-col w-1/3">
-                <h1 className="font-bold text-2xl">Appearance</h1>
-                <p className="font-light text-copy-light mb-2">Change the appearance of the app</p>
-                <div className="flex flex-row mt-3 bg-foreground p-3 rounded-lg gap-3">
-                    <button onClick={() => setTheme('light')} className="w-8 h-8 rounded-full bg-[#edecf3] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
-                    <button onClick={() => setTheme('dark')} className="w-8 h-8 rounded-full bg-[#17161d] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
-                    <button onClick={() => setTheme('discordish')} className="w-8 h-8 rounded-full bg-[#131420] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
-                    <button onClick={() => setTheme('bluey')} className="w-8 h-8 rounded-full bg-[#111d22] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
-                    <button onClick={() => setTheme('reptile')} className="w-8 h-8 rounded-full bg-[#141f18] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
-                </div>
-                
-            </div>
+      <div className="mx-10 flex flex-row items-center justify-between border-b border-border pb-3">
+        <h1 className="font-bold text-xl">Settings</h1>
+        <p className="font-light text-copy-light">Personalize your experience and manage your preferences</p>
+      </div>
 
-            <Navbar handleCreateList={handleCreateList}/>
+      <div className="px-10 mt-8 flex flex-col w-1/3">
+        <h1 className="font-bold text-2xl">Appearance</h1>
+        <p className="font-light text-copy-light mb-2">Change the appearance of the app</p>
+        <div className="flex flex-row mt-3 bg-foreground p-3 rounded-lg gap-3">
+          <button onClick={() => handleSetTheme('light')} className="w-8 h-8 rounded-full bg-[#edecf3] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
+          <button onClick={() => handleSetTheme('dark')} className="w-8 h-8 rounded-full bg-[#17161d] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
+          <button onClick={() => handleSetTheme('discordish')} className="w-8 h-8 rounded-full bg-[#131420] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
+          <button onClick={() => handleSetTheme('bluey')} className="w-8 h-8 rounded-full bg-[#111d22] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
+          <button onClick={() => handleSetTheme('reptile')} className="w-8 h-8 rounded-full bg-[#141f18] border border-border hover:border-primary hover:border-2 hover:-translate-y-1 transition-transform duration-200"/>
         </div>
-    )
+      </div>
+
+      <Navbar handleCreateList={handleCreateList}/>
+    </div>
+  );
 }
 
 export default Settings;

7154e4b9fc13dd44e1baf09409cc87bf0ef70bf7
Wes <wes@miragecorp.org>
2024-11-24 22:53:59 -0500

    turned nav into component to prep for settings page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { HomeIcon, Settings } from "lucide-react"; 
+import { NavLink } from 'react-router-dom';
+
+const Navbar = ({ handleCreateList }) => {
+    return (
+    <div id='nav' className='absolute bottom-0 w-screen flex flex-row justify-between bg-foreground items-center px-10 py-5 border-t border-border'>
+        <div className='flex flex-row items-center gap-4'>
+            <NavLink to="/" className={({ isActive }) => isActive ? 'rounded-lg bg-border px-3 py-2 text-primary duration-200 flex flex-row gap-1 items-center' : 'rounded-lg bg-border px-3 py-2 hover:text-primary duration-200 flex flex-row gap-1 items-center'}>
+                <HomeIcon className="w-5 h-5" />
+                <h1 className="text-m font-medium">Home</h1>
+            </NavLink>
+            <NavLink to="/settings" className={({ isActive }) => isActive ? 'rounded-lg bg-border px-3 py-2 text-primary duration-200 flex flex-row gap-1 items-center' : 'rounded-lg bg-border px-3 py-2 hover:text-primary duration-200 flex flex-row gap-1 items-center'}>
+                <Settings className="w-5 h-5" />
+                <h1 className="text-m font-medium">Settings</h1>
+            </NavLink>
+        </div>
+
+        <button onClick={() => handleCreateList()} className='text-foreground bg-gradient-to-r from-primary to-secondary rounded-md font-semibold px-6 py-2 hover:-translate-y-1 transition-transform duration-200'>
+            Create New List
+        </button>
+    </div>
+    )
+}
+
+export default Navbar;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { HomeIcon, Plus, Settings } from "lucide-react"; 
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useList } from '../../context/ListContext';
 import List from '../../components/List';
 import Flow from '../../components/Flow';
+import Navbar from '../../components/Navbar';
 
 const Home = () => {    
     const { lists, setCurrentList, createNewList } = useList();
@@ -30,35 +30,20 @@ const Home = () => {
                 <p className="font-light text-copy-light">Lists with your upcoming tasks</p>
             </div>
             
-            <div className="px-10 my-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-y-scroll">
+            <div className="px-10 mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-y-scroll">
                 {lists.map((list) => (
                     <List key={list.id} list={list} handleLoadList={handleLoadList} />
                 ))}
 
-                <div className="flex flex-col items-center justify-center">
+                {/* <div className="flex flex-col items-center justify-center">
                     <button onClick={handleCreateList} className="duration-200 hover:border-primary hover:text-primary items-center justify-center flex flex-col bg-foreground border-2 border-border border-dashed rounded-lg w-full h-80 p-3">
                         <Plus className="w-8 h-8 mb-3" />
                         <h1 className="text-xl font-bold">CREATE LIST</h1>
                     </button>
-                </div>
+                </div> */}
             </div>
 
-            <div id='nav' className='w-screen flex flex-row justify-between bg-foreground items-center px-10 py-5 border-t border-border'>
-                <div className='flex flex-row items-center gap-4'>
-                    <NavLink to="/" className="rounded-lg bg-border px-3 py-2 hover:text-primary duration-200 flex flex-row gap-1 items-center">
-                        <HomeIcon className="w-5 h-5" />
-                        <h1 className="text-m font-medium">Home</h1>
-                    </NavLink>
-                    <NavLink to="/settings" className="rounded-lg bg-border px-3 py-2 hover:text-primary duration-200 flex flex-row gap-1 items-center">
-                        <Settings className="w-5 h-5" />
-                        <h1 className="text-m font-medium">Settings</h1>
-                    </NavLink>
-                </div>
-
-                <button className='text-foreground bg-gradient-to-r from-primary to-secondary rounded-md font-semibold px-6 py-2 hover:-translate-y-1 transition-transform duration-200'>
-                    Create Quick List
-                </button>
-            </div>
+            <Navbar handleCreateList={handleCreateList}/>
         </div>
     );
 }

5ea3dcd8c2610334c9fa8eb48318289aee80e4b3
Wes <wes@miragecorp.org>
2024-12-02 21:16:09 -0600

    scroll fix for lists

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -30,17 +30,10 @@ const Home = () => {
                 <p className="font-light text-copy-light">Lists with your upcoming tasks</p>
             </div>
             
-            <div className="px-10 mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-y-scroll no-scrollbar">
+            <div className="px-10 mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-y-scroll no-scrollbar pb-24">
                 {lists.map((list) => (
                     <List key={list.id} list={list} handleLoadList={handleLoadList} />
                 ))}
-
-                {/* <div className="flex flex-col items-center justify-center">
-                    <button onClick={handleCreateList} className="duration-200 hover:border-primary hover:text-primary items-center justify-center flex flex-col bg-foreground border-2 border-border border-dashed rounded-lg w-full h-80 p-3">
-                        <Plus className="w-8 h-8 mb-3" />
-                        <h1 className="text-xl font-bold">CREATE LIST</h1>
-                    </button>
-                </div> */}
             </div>
 
             <Navbar handleCreateList={handleCreateList}/>
